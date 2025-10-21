f1d2c4e0f6536b566b0766abd5de260ffabe1e51
Mahesh Kumar <mahesh@Maheshs-MacBook-Air.local>
2025-10-21 19:26:30 +0530

    continuing

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ dotenv.config();  // <-- no path unless you renamed file to env
 
 import connectDB from "./db/index.js";
 
-connectDB();
-
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT||8000,()=>{
+     console.log(`Server is ruuning at PORT${process.env.PORT}`)
+  })
+})
+.catch((error)=>{
+  console.log("MONGODB failed to connect",error);
+})
 
 // (async()=>{
 //     try{
